41806fb30c4258e133ed5f2d333b941c8073ff0e
saranyams057 <61340715+saranyams057@users.noreply.github.com>
2023-02-26 19:33:53 +0530

    Delete travelproject directory

--- travelproject/travel/travelapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from . models import places
-admin.site.register(places)
-
-from . models import team
-admin.site.register(team)
--- travelproject/travel/travelapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from . import views
-from django.urls import path
-
-urlpatterns = [
-
-    path('', views.demo, name='demo')
-]
